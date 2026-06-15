c444fb01fb8b9dbef3444ad8d288c4dae3888443
ethancha0 <ethanchao2005@gmail.com>
2026-06-14 18:42:56 -0700

    style: animate title text

--- app/sections/landing.tsx
+++ app/sections/landing.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import Image from "next/image"
 import Typography from '@mui/material/Typography';
+import VariableProximity from "@/components/VariableProximity";
+import { useRef } from 'react';
+
+
 
 
 export default function Landing(){
-
+    const containerRef = useRef(null);
     return(
         <div className="flex justify-center w-screen mt-70">
 
@@ -14,9 +20,37 @@ export default function Landing(){
                 </div>
 
                 <div className="flex flex-col items-center">
-                    <Typography variant="h3">Networking</Typography>
-                    <Typography variant="h3">Night</Typography>
-                </div>                
+
+                 <Typography>ICSSC Presents</Typography>
+
+                    <div ref={containerRef} className="relative">
+                        <VariableProximity
+                            style={{ fontSize: "4rem" }}
+                            label="Networking"
+                            className={'variable-proximity-demo'}
+                            fromFontVariationSettings="'wght' 400, 'opsz' 9"
+                            toFontVariationSettings="'wght' 1000, 'opsz' 40"
+                            containerRef={containerRef}
+                            radius={100}
+                            falloff="linear"
+                        />
+                    </div>
+
+                    <div ref={containerRef} className="relative">
+
+                        <VariableProximity
+                            style={{ fontSize: "4rem" }}
+                            label="Night"
+                            className={'variable-proximity-demo'}
+                            fromFontVariationSettings="'wght' 400, 'opsz' 9"
+                            toFontVariationSettings="'wght' 1000, 'opsz' 40"
+                            containerRef={containerRef}
+                            radius={100}
+                            falloff="linear"
+                        />
+                    </div>
+
+                    </div>                
             </div>
 
             
---
+++ lib/fonts.ts
@@ -0,0 +1,6 @@
+import { Roboto_Flex } from "next/font/google";
+
+export const robotoFlex = Roboto_Flex({
+  subsets: ["latin"],
+  display: "swap",
+});
